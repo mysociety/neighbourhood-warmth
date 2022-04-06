be2e8f251868f805195556dd91801b5322e889ff
Zarino Zappia <mail@zarino.co.uk>
2022-04-06 16:01:54 +0100

    Skeleton Jekyll site

---
+++ static/js/all.js
@@ -0,0 +1,7 @@
+import * as _ from '../vendor/underscore/js/underscore.esm.min.js'
+import $ from '../vendor/jquery/js/jquery.esm.js'
+
+$(function(){
+    console.log('jQuery ready')
+    console.log('Underscore.now()', _.now())
+})
---
+++ static/vendor/bootstrap/js/index.esm.js
@@ -0,0 +1,34 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.1.3): index.esm.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from './src/alert.js'
+import Button from './src/button.js'
+import Carousel from './src/carousel.js'
+import Collapse from './src/collapse.js'
+import Dropdown from './src/dropdown.js'
+import Modal from './src/modal.js'
+import Offcanvas from './src/offcanvas.js'
+import Popover from './src/popover.js'
+import ScrollSpy from './src/scrollspy.js'
+import Tab from './src/tab.js'
+import Toast from './src/toast.js'
+import Tooltip from './src/tooltip.js'
+
+export {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
