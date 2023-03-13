770ee170316b970d4577e18c822086acbb825893
Struan Donald <struan@exo.org.uk>
2023-03-13 16:33:53 +0000

    Remove static site files

--- static/js/all.js
+++
@@ -1,7 +0,0 @@
-import * as _ from '../vendor/underscore/js/underscore.esm.min.js'
-import $ from '../vendor/jquery/js/jquery.esm.js'
-
-$(function(){
-    console.log('jQuery ready')
-    console.log('Underscore.now()', _.now())
-})
