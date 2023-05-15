0c9975a864fccdc224b91c90afd87d07267ff0f1
Struan Donald <struan@exo.org.uk>
2023-05-15 13:46:51 +0100

    add slug to team model
    with pre-save signal to update the slug

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -3,6 +3,9 @@ from django.contrib.gis import measure
 from django.contrib.gis.db import models
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 
 class User(AbstractUser):
@@ -13,6 +16,7 @@ class Team(models.Model):
     name = models.CharField(max_length=100)
     base_pc = models.CharField(max_length=10)
     centroid = models.PointField()
+    slug = models.CharField(max_length=100, blank=True, null=True, default="")
 
     @classmethod
     def find_nearest_groups(self, latitude=None, longitude=None, distance=5):
@@ -28,3 +32,20 @@ class Team(models.Model):
         )
 
         return nearest
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=[
+                    "slug",
+                ]
+            )
+        ]
+
+
+@receiver(pre_save, sender=Team)
+def generate_team_slug(sender, instance, raw, using, update_fields, **kwargs):
+    slug = instance.slug
+    if slug is None:
+        slug = slugify(instance.name)
+        instance.slug = slug
